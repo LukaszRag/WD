1eb8c49a3b891638a2562628b6a0a6079d4192e3
LukaszRag <93649748+LukaszRag@users.noreply.github.com>
2022-05-21 11:36:50 +0200

    Update zad2.1.py

--- zad2.1.py
+++ zad2.1.py
@@ -6,7 +6,7 @@ df = pd.read_excel("sport.xlsx", header=None)
 x = df.iloc[:, 1]
 etykiety = df.iloc[:, 0]
 plt.pie(x, labels=etykiety, autopct='%1.0f%%', explode=(0.1,0,0,0,0,0))
-plt.annotate('Dawid Końpa', [-1.2, 1])
+plt.annotate()
 plt.title("Popularność sportów w grupie nastolatków")
 plt.savefig("zad2.jpg", format='jpg')
 plt.show()
